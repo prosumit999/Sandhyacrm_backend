452dbd8f847e6a7b14eddbec4d781b12518424eb
sumit <prosumit999@gmail.com>
2026-07-06 10:21:25 +0530

    updated db config

--- config/db.config.js
+++ config/db.config.js
@@ -2,10 +2,7 @@ const mongoose = require("mongoose")
 const databaseConnection = async () => {
    try {
      const uri = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/sandhya_crm"
-     await mongoose.connect(uri, {
-       useNewUrlParser: true,
-       useUnifiedTopology: true,
-     })
+     await mongoose.connect(uri)
      console.log("Database is connected:", mongoose.connection.name)
    } catch (err) {
      console.error("Error connecting to the database:", err.message)
